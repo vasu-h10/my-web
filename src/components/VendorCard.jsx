e66582c50f66d149118ce65c9db0a35a33018fca
Vasu-H <195313818+vasu-h10@users.noreply.github.com>
2025-11-05 17:23:10 +0530

    Update VendorCard.jsx

--- src/components/VendorCard.jsx
+++ src/components/VendorCard.jsx
@@ -7,13 +7,8 @@ import VendorSourceCard from "./VendorSourceCard";
 export default function VendorCard({ vendor }) {
   return (
     <div className="vendor-card">
-      {/* 1. Vendor Header */}
       <VendorHeader vendor={vendor} />
-
-      {/* 2. Vendor Dishes */}
       <VendorDishes dishes={vendor.dishes || []} />
-
-      {/* 3. Vendor Source Card */}
       <VendorSourceCard vendor={vendor} />
     </div>
   );
