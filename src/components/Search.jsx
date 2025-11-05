b3d8a77ed6db3b171f2e4e756d18f43480e2cc0a
Vasu-H <195313818+vasu-h10@users.noreply.github.com>
2025-11-05 14:05:40 +0530

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,6 +4,22 @@ import "./Search.css";
 export default function Search({ value, onChange }) {
   return (
     <div className="search-container">
+      <span className="search-icon">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          viewBox="0 0 24 24"
+        >
+          <circle cx="11" cy="11" r="8"></circle>
+          <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+        </svg>
+      </span>
       <input
         type="text"
         placeholder="Search vendors..."
