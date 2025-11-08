4c9bd767612758b4217451fb407a7949fb409617
vasu-h10 <vasu-h10@users.noreply.github.com>
2025-11-08 06:39:07 +0530

    Update VendorCard.jsx to use vendor prop and updateVendor callback

--- src/components/VendorCard.jsx
+++ src/components/VendorCard.jsx
@@ -4,11 +4,12 @@ import VendorHeader from "./VendorHeader";
 import VendorDishes from "./VendorDishes";
 import VendorSourceCard from "./VendorSourceCard";
 
-export default function VendorCard({ vendor }) {
+export default function VendorCard({ vendor, updateVendor }) {
   return (
     <div className="vendor-card">
       <VendorHeader vendor={vendor} />
-      <VendorDishes dishes={vendor.dishes || []} />
+      {/* Pass updateVendor so VendorDishes can persist changes */}
+      <VendorDishes vendor={vendor} updateVendor={updateVendor} />
       <VendorSourceCard vendor={vendor} />
     </div>
   );
