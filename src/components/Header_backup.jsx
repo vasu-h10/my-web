61fbc75dbd5555a516c315d003c33f7e87967078
vasu-h10 <vasu-h10@users.noreply.github.com>
2025-11-15 18:23:14 +0530

    Clean cache for Render deployment

---
+++ src/components/Header_backup.jsx
@@ -0,0 +1,17 @@
+import DonateIcon from "./icons/DonateIcon";
+import React from "react";
+import LogoTitle from "./icons/LogoTitle";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header-item">👤</div>
+      <div className="header-item logo-item">
+        <LogoTitle width={50} />
+      </div>
+      <div className="header-item">🌓</div>
+      <div className="header-item">💝</div>
+    </header>
+  );
+}
