0ebee86b33fa61337ec578c5ffa595924cd40b30
vasu-h10 <vasu-h10@users.noreply.github.com>
2025-11-15 18:35:43 +0530

    Add DonateIcon to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,11 +7,18 @@ export default function Header() {
   return (
     <header className="header">
       <div className="header-item">👤</div>
+      
       <div className="header-item logo-item">
         <LogoTitle width={50} />
       </div>
+      
       <div className="header-item">🌓</div>
       <div className="header-item">💝</div>
+      
+      {/* Add the donation icon here */}
+      <div className="header-item">
+        <DonateIcon width={120} height={150} />
+      </div>
     </header>
   );
 }
